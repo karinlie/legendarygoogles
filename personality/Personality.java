638039bcfca58c10b19ab75eb4163ee8d27e961b
magnetenstad <tenstad.magne@gmail.com>
2021-03-27 19:32:47 +0100

    Add items to Personality

--- personality/Personality.java
+++ personality/Personality.java
@@ -1,7 +1,11 @@
 package personality;
 
+import java.util.Collection;
 import describable.Describable;
+import item.Item;
 
 public interface Personality extends Describable {
-	
+
+	Collection<Item> getItems();
+
 }
