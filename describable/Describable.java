8169d85926ab9bfbfda4834f357137e530cb1ddf
magnetenstad <tenstad.magne@gmail.com>
2021-03-27 19:33:00 +0100

    Add static describe method

--- describable/Describable.java
+++ describable/Describable.java
@@ -1,7 +1,11 @@
 package describable;
 
 public interface Describable {
-	
+
+	public static String describe(Describable describable) {
+		return describable.getName() + ": " + describable.getDescription();
+	}
+
 	public String getName();
 
 	public String getDescription();
