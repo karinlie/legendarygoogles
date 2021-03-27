ef1a318e2502deb9240d82b66332233182bcdc9e
magnetenstad <tenstad.magne@gmail.com>
2021-03-27 19:41:41 +0100

    Describe whole playground

--- Main.java
+++ Main.java
@@ -5,7 +5,19 @@ import describable.*;
 
 class Main {
 	public static void main(String[] args) {
-		Describable playground = new Playground();
+		Location playground = new Playground();
 		System.out.println(Describable.describe(playground));
+
+		for (Personality personality : playground.getPersonalities()) {
+			System.out.println(Describable.describe(personality));
+
+			for (Item item : personality.getItems()) {
+				System.out.println(Describable.describe(item));
+			}
+		}
+
+		for (Item item : playground.getItems()) {
+			System.out.println(Describable.describe(item));
+		}
 	}
 }
